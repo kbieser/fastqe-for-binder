e9d5506d716a881028807c59839f2e2ebe7f82b1
Jason Williams <JasonJWilliamsNY@gmail.com>
2020-05-12 12:59:21 -0400

    update entrypoint stuff

--- entry.sh
+++ entry.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec jupyter lab --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --notebook-dir='/home/cyverse_user' --Session.username='cyverse_user'
+exec jupyter lab --NotebookApp.token='' --NotebookApp.password='' --notebook-dir='/home/cyverse_user' --Session.username='cyverse_user'
