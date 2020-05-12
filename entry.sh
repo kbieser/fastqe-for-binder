ed92ed3492b4584ff4a2bc2a45e073eabd564b79
Jason Williams <JasonJWilliamsNY@gmail.com>
2020-05-12 12:36:38 -0400

    update entrypoint stuff

--- entry.sh
+++ entry.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
-
-exec jupyter lab --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --notebook-dir='/home/cyverse_user' --Session.username='cyverse_user' --jupyterLabRmotrSolutions.is_enabled='true' --jupyterLabRmotrSolutions.role='teacher'
+exec jupyter lab --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --notebook-dir='/home/cyverse_user' --Session.username='cyverse_user'
